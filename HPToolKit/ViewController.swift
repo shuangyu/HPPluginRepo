6097427deda1623a52020777c7f1fbc3902fb771
Hu Peng <peng.hu@sap.com>
2017-11-30 14:24:25 +0800

    Initial Commit

---
+++ HPToolKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HPToolKit
+//
+//  Created by Hu, Peng on 30/11/2017.
+//  Copyright © 2017 Hu, Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
